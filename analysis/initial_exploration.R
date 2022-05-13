d0dfff9154a7bf30cc409d06e800a2707791de61
Rob Moss <robm.dev@gmail.com>
2022-05-13 11:18:47 +1000

    Update script to analyse both data sets
    The analysis script now summarises each data set, and saves the results
    in a single CSV file.

--- analysis/initial_exploration.R
+++ analysis/initial_exploration.R
@@ -1,20 +1,31 @@
 #!/usr/bin/Rscript --vanilla
 
-input_file <- file.path('input_data', 'data.csv')
+input_files <- c(
+    file.path('input_data', 'first_data_set.csv'),
+    file.path('input_data', 'second_data_set.csv'))
 output_file <- file.path('outputs', 'summary.csv')
 
-data <- read.csv(input_file, header = TRUE)
+summarise_data_file <- function(input_file) {
+    data <- read.csv(input_file, header = TRUE)
 
-summary_statistics <- list(
-    mean = mean(data$value),
-    median = median(data$value),
-    minimum = min(data$value),
-    maximum = max(data$value),
-    first = data$value[1],
-    last = data$value[nrow(data)])
+    summary_statistics <- list(
+        mean = mean(data$value),
+        median = median(data$value),
+        minimum = min(data$value),
+        maximum = max(data$value),
+        first = data$value[1],
+        last = data$value[nrow(data)])
 
-summary_table <- data.frame(
-    statistic = names(summary_statistics),
-    value = as.numeric(summary_statistics))
+    summary_table <- data.frame(
+        data_set = basename(input_file),
+        statistic = names(summary_statistics),
+        value = as.numeric(summary_statistics))
+
+    summary_table
+}
+
+summary_tables <- lapply(input_files, summarise_data_file)
+
+summary_table <- do.call(rbind, summary_tables)
 
 write.csv(summary_table, output_file, quote = FALSE, row.names = FALSE)
